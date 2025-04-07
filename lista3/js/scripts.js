dd25ba252d9a67d7f835863b3f6f528a390f7dd1
silvioperoni <silviocezar08@gmail.com>
2025-04-07 17:42:25 -0300

    lista 3

--- lista3/js/scripts.js
+++ lista3/js/scripts.js
@@ -35,5 +35,59 @@ function exe2(){
   }
   document.getElementById("tabela").innerHTML = saida
   }
-    
-     
+
+  function exe3(){
+    let faixa1 = 0,faixa2 = 0,faixa3 = 0,faixa4 = 0,faixa5 = 0
+    let idade
+    // estrutura de repeyição for
+    // let i = 1 será executado uma unica vez, logo no inicio
+    // i <= 8 será executado toda vez, antes de entrar no looping
+    // i++ será executado toda vez, antes de ir para a próxima interação
+    for (let i = 1; i <= 8; i++){
+        idade = Number(prompt(`Informe a idade ${i}`))
+        if (idade >=0 &&  idade <= 15){
+            faixa1++
+        }
+        else if (idade >15 &&  idade <= 30){
+            faixa2++
+        }
+        else if (idade > 30 &&  idade <= 45){
+            faixa3++
+        } 
+        else if (idade > 45 &&  idade <= 60){
+            faixa4++ 
+        }
+        else if (idade > 60){
+            faixa5++
+        }
+        else {
+            alert(`ìdade negativa`)
+        }
+    }
+        document.getElementById("resultado").innerHYML =
+            `F1: ${faixa1} <br/>F2: ${faixa2} <br/>F3: ${faixa3} <br/>
+             F4: ${faixa4} <br/>F5: ${faixa5} <br/> %F1:{faixa1/8*100}
+             %F5:{faixa5/8*100}%`
+}
+
+function exe4(){
+    // recuperar o numero fornecido
+    let numero = Number(document.getElementById("numero").value)
+    let resultado  = ""
+    // estrutura de repetição para calcular 
+    for(let i = 0; i <= 10; i++){
+        resultado +=`${numero} * ${i} = ${numero * i} <br/>`
+   
+    document.getElementById("resultado").innerHTML = resultado
+}
+
+}function exe5(){
+    let resultado = ""
+    for(let numero = 1; numero <= 10; numero++){
+        resultado += `Tabuada do ${numero} <br/>`
+        for(let i = 0; i <= 10; i++)
+            resultado += ${numero} * ${i} = ${numero*i} <br/>
+    }
+} 
+  document.getElementById("resultado2").innerHTML = resultado
+}
