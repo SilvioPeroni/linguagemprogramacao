7f1eac6847901f9b9c113248027b6cdc3abc15ad
silvioperoni <silviocezar08@gmail.com>
2025-09-03 18:31:31 -0300

    adiciona listajack.js

--- listajack.js/exercjackinsercao.js
+++ listajack.js/exercjackinsercao.js
@@ -1,9 +1,22 @@
 // ordene os números 25 57 48 37 12 92 86 33 
-function selecaoDireta(vet) {
-  for(let i = 0; i < vet.length; i++){
-    for(let j = i + 1; j < vet.length; j++){
-      
+function insercaoDireta(vet) {
+  
+  for(let i = 1; i < vet.length; i++){
+    for(let j = 0; j < i; j++){
+      if (vet[i] < vet[j]){
+        var aux= vet[i]
+        for(var k = i; k > j; k--){
+          vet[k] = vet[k-1]
+        }
+        vet[j] = aux
+      }
+        
     }
+   
   }
 
 }
+
+let numeros = [25, 57, 48, 37, 12, 92, 86, 33];
+let ordenado = insercaoDireta(numeros);
+console.log("Vetor ordenado:", ordenado);
